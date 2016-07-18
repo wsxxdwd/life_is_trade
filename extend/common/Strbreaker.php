4b1a1705ad9ca1425e377ae4ebed487756f404ec
BoundlessHaung <george.haung@sandboxcn.com>
2016-07-18 11:13:49 +0800

    增加简单分词方法
    增加简单分词方法

---
+++ extend/common/Strbreaker.php
@@ -0,0 +1,22 @@
+<?php
+namespace common;
+
+class Strbreaker
+{
+    /**
+     * 简单字符串分词
+     * @param  string $str 需要分词的字符串
+     * @return array
+     */
+    public static function strbreaker($str = '') {
+        $strlen = mb_strlen($str);
+        $strarr = array();
+        for ($sublen = 1; $sublen < $strlen; $sublen++) {
+            $aclen = $strlen - ($sublen - 1);
+            for ($start = 0; $start < $aclen; $start++) { 
+                $strarr[] = mb_substr($str, $start, $sublen);
+            }
+        }
+        return $strarr;
+    }
+}

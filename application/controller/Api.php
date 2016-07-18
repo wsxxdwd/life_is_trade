9d58a06bc45450e33466c136eba142e525249f87
BoundlessHaung <george.haung@sandboxcn.com>
2016-07-18 17:05:55 +0800

    搜索
    搜索

--- application/controller/Api.php
+++ application/controller/Api.php
@@ -5,6 +5,8 @@ use think\Validate;
 use app\model\News;
 use app\model\Tradeinfo;
 use app\model\Tradeiteminfo;
+use common\Strbreaker;
+use think\Db;
 
 class Api
 {
@@ -41,7 +43,7 @@ class Api
      */
     public function addtradeinfo()
     {
-        // 判断所需参数是否全部存在如果有确实则结束
+        // 判断所需参数是否全部存在如果有缺失则结束
         if (!isset($_POST['title']) ||
             !isset($_POST['trader']) ||
             !isset($_POST['onlinetime']) ||
@@ -114,7 +116,7 @@ class Api
      */
     public function gettradeinfo()
     {
-        // 判断参数是否存在不存在则结束
+        // 判断所需参数是否全部存在如果有缺失则结束
         if (!isset($_GET['lastid']) || !isset($_GET['limit']) || !isset($_GET['type'])) {
             return [
                 'status' => 0,
@@ -131,7 +133,7 @@ class Api
             $exp = '<';
         }
         // 获取交易信息列表
-        if ($_GET['lastid'] == 'false') {
+        if ($_GET['lastid'] == 'false' || $_GET['lastid'] == '') {
             $_GET['lastid'] = false;
         }
         if ($_GET['lastid'] && ($_GET['type'] == 1 || $_GET['type'] == 0)) {
@@ -184,7 +186,7 @@ class Api
      */
     public function addnew()
     {
-        // 判断所需参数是否全部存在如果有确实则结束
+        // 判断所需参数是否全部存在如果有缺失则结束
         if (!isset($_POST['title']) || !isset($_POST['creatusername']) || !isset($_POST['content'])) {
             return [
                 'status' => 0,
@@ -222,7 +224,7 @@ class Api
      */
     public function getnews()
     {
-        // 判断所需参数是否全部存在如果有确实则结束
+        // 判断所需参数是否全部存在如果有缺失则结束
         if (!isset($_GET['limit']) || !isset($_GET['lastid'])) {
             return [
                 'status' => 0,
@@ -230,7 +232,7 @@ class Api
             ];
         }
         // 获取新闻列表
-        if ($_GET['lastid'] == 'false') {
+        if ($_GET['lastid'] == 'false' || $_GET['lastid'] == '') {
             $_GET['lastid'] = false;
         }
         if ($_GET['lastid']) {
@@ -251,6 +253,93 @@ class Api
                 'status' => 0,
                 'msg' => '新闻获取失败'
             ];
+        }
+    }
+
+    public function search()
+    {
+        // 判断所需参数是否全部存在如果有缺失则结束
+        if (!isset($_GET['wd']) || !isset($_GET['limit']) || !isset($_GET['lastid']) || !isset($_GET['type'])) {
+            return [
+                'status' => 0,
+                'msg' => '传入参数错误'
+            ];
+        } else {
+            if ($_GET['wd'] = '' || mb_strlen($_GET['wd']) > 30) {
+                return [
+                    'status' => 0,
+                    'msg' => '关键字格式错误'
+                ];
+            }
+            if ($_GET['lastid'] == 'false' || $_GET['lastid'] == '') {
+                $_GET['lastid'] = false;
+            }
+            // 对关键字进行简单分词
+            $strarr = Strbreaker::strbreaker($_GET['wd']);
+            // 生成SQL语句，并查询数据
+            $strarrlen = count($strarr);
+            $condition = '';
+            for ($i = 0; $i < $strarrlen; $i++) { 
+                if ($i == 0) {
+                    $condition = "itemname LIKE '%" . $strarr[$i] . "%' ";
+                } else {
+                    $condition .= "OR itemname LIKE '%" . $strarr[$i] . "%' ";
+                }
+            }
+            $Tradeiteminfo = Db::query("select * from lit_tradeiteminfo where (" . $condition . ")");
+            // 如果找到数据，计算相似度并按照相似度排序
+            if (is_array($Tradeiteminfo)) {
+                // 获取交易信息
+                $Tradeiteminfolen = count($Tradeiteminfo);
+                for ($j = 0; $j < $Tradeiteminfolen; $j++) { 
+                    $keylist[] = $Tradeiteminfo[$j]['tid'];
+                }
+                $Tradeinfo = Tradeinfo::all($keylist);
+                // 判断是否成功获取交易信息列表如果成功获取便获取物品信息
+                if (is_array($Tradeinfo)) {
+                    // 获取物品列表，并计算相似度
+                    $volume = array();
+                    foreach ($Tradeinfo as $key => $data) {
+                        $items = Tradeiteminfo::where('tid=' . $data['tid'])->order('iid')->select();
+                        if (is_array($items)) {
+                            $Tradeinfo[$key]['items'] = $items;
+                            $percent = 0;
+                            $Tradeinfo[$key]['percent'] = 0;
+                            // 计算相似度
+                            foreach ($items as $itemidx => $item) {
+                                similar_text($items[$itemidx]['itemname'], $_GET['wd'], $percent);
+                                // 只存入更大的值
+                                if ($percent > $Tradeinfo[$key]['percent']) {
+                                    $Tradeinfo[$key]['percent'] = $percent;
+                                    $volume[$key] = $percent;
+                                }
+                            }
+                        } else {
+                           return [
+                                'status' => 0,
+                                'msg' => '搜索失败'
+                            ];
+                        }
+                    }
+                    // 根据相似度排序并输出数据
+                    array_multisort($volume, SORT_DESC, $Tradeinfo);
+                    return [
+                        'status' => 1,
+                        'msg' => '成功获取新闻',
+                        'data' => $Tradeinfo
+                    ];
+                } else {
+                    return [
+                        'status' => 0,
+                        'msg' => '搜索失败'
+                    ];
+                }
+            } else {
+                return [
+                    'status' => 0,
+                    'msg' => '搜索失败'
+                ];
+            }
         }
     }
 
--- application/controller/Search.php
+++ application/controller/Search.php
@@ -32,29 +32,38 @@ class Search extends Controller
             $Tradeiteminfo = Db::query("select * from lit_tradeiteminfo where (" . $condition . ")");
             // 如果找到数据，计算相似度并按照相似度排序
             if (is_array($Tradeiteminfo)) {
-                foreach ($Tradeiteminfo as $key => $row) {
-                    similar_text($Tradeiteminfo[$key]['itemname'], $_GET['wd'], $Tradeiteminfo[$key]['percent']);
-                    $volume[$key]  = $Tradeiteminfo[$key]['percent'];
-                }
-                array_multisort($volume, SORT_DESC, $Tradeiteminfo);
                 // 获取交易信息
                 $Tradeiteminfolen = count($Tradeiteminfo);
                 for ($j = 0; $j < $Tradeiteminfolen; $j++) { 
                     $keylist[] = $Tradeiteminfo[$j]['tid'];
                 }
-                $Tradeinfo = Tradeinfo::all($keylist);
+                $Tradeinfo = Tradeinfo::order('createtime')->all($keylist);
                 // 判断是否成功获取交易信息列表如果成功获取便获取物品信息
                 if (is_array($Tradeinfo)) {
-                    // 获取物品列表
+                    // 获取物品列表，并计算相似度
+                    $volume = array();
                     foreach ($Tradeinfo as $key => $data) {
-                        $item = Tradeiteminfo::where('tid=' . $data['tid'])->order('iid')->select();
-                        if (is_array($item)) {
-                            $Tradeinfo[$key]['items'] = $item;
+                        $items = Tradeiteminfo::where('tid=' . $data['tid'])->order('iid')->select();
+                        if (is_array($items)) {
+                            $Tradeinfo[$key]['items'] = $items;
+                            $percent = 0;
+                            $Tradeinfo[$key]['percent'] = 0;
+                            // 计算相似度
+                            foreach ($items as $itemidx => $item) {
+                                similar_text($items[$itemidx]['itemname'], $_GET['wd'], $percent);
+                                // 只存入更大的值
+                                if ($percent > $Tradeinfo[$key]['percent']) {
+                                    $Tradeinfo[$key]['percent'] = $percent;
+                                    $volume[$key] = $percent;
+                                }
+                            }
                         } else {
                            $this->assign('Tradeinfo', false);
                            return $this->fetch();
                         }
                     }
+                    // 根据相似度排序并输出数据
+                    array_multisort($volume, SORT_DESC, $Tradeinfo);
                     $this->assign('Tradeinfo', $Tradeinfo);
                 } else {
                     $this->assign('Tradeinfo', false);
